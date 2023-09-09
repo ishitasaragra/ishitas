92a7cfcd321239d455f75bebec7c2dfbbc1cb68a
ishitasaragra <saragraishita@gmail.com>
2023-09-09 16:27:56 +0530

    changes

---
+++ Assignment/Html form.php
@@ -0,0 +1,23 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>User Details form</title>
+<fieldset>
+
+    
+    <h1 style="color:red;background-color:green; text-transform:uppercase;text align:center">Step 1: Your details</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        
+	     Name:<input type="text" name="name" placeholder="first and last name" required>
+       <br>
+       <br>
+        Email:<input type="email" name="email" placeholder="example@domain.com" >
+        <br>
+        <br>
+        Phone_no :<input type="Phone_no" name="phone_no" placeholder="Eg. +447500000000">
+
+    </form>
+
+</fieldset>
+</head>
+ </html>
